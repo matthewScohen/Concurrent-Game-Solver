d71cb9e59ab545471ec4bb5be312ab971bb5ab05
Sumukha Udupa <sudupa@umich.edu>
2024-07-29 18:03:44 -0400

    added sub-assignments.

--- ConcurrentEnvironment/State.py
+++ ConcurrentEnvironment/State.py
@@ -7,6 +7,10 @@ class State:
         # out_nodes[action][action2] = list(State)
         self.in_neighbors = dict()
 
+        # Sub-assignment dictionary for the traditional solver: defining it as a dictionary of sets.
+        self.sub_assignment_p1 = set()
+        self.sub_assignment_p2 = set()
+
     def add_in_node(self, state: 'State', action1: str, action2: str):
         if action1 not in self.in_neighbors:
             self.in_neighbors[action1] = dict()
@@ -21,3 +25,10 @@ class State:
             self.out_neighbors[action1][action2] = dict()
         self.out_neighbors[action1][action2] = state
 
+    def add_sub_assignment(self, state: 'State', action1: str, action2: str):
+
+        if action1 not in self.sub_assignment_p1:
+            self.sub_assignment_p1.add(action1)
+
+        if action2 not in self.sub_assignment_p2:
+            self.sub_assignment_p2.add(action2)
