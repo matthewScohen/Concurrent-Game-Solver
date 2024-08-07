11fbeeca254612c35efbb4aa266bbcd4e08e35ea
Sumukha Udupa <sudupa@umich.edu>
2024-08-06 23:12:51 -0400

    Fixed bugs and the code follows the pseudo code in the paper closely.

--- Solvers/traditional_solver.py
+++ Solvers/traditional_solver.py
@@ -7,12 +7,12 @@ def traditional_solver(graph: Graph, final_states: set[str]) -> (set[State], dic
     # winning_region = set()
     # strategy = dict()
 
-    winning_region = almost_sure_solver(graph, final_states)
+    winning_region, strategy_1 = almost_sure_solver(graph, final_states)
 
-    return winning_region
+    return winning_region, strategy_1
 
 
-def stay(player: int, state: 'State', target_set):
+def stay(player: int, state: str, target_set, graph: Graph, gamma_1: dict, gamma_2: dict):
     stay_action_set = set()
     # if player == 1:
     #     for a1 in state.sub_assignment_p1:
@@ -24,9 +24,18 @@ def stay(player: int, state: 'State', target_set):
     #
     #         if flag == 0:
     #             stay_set.add(a1)
+    # if player == 1:
+    #     for a1 in state.sub_assignment_p1:
+    #         if all(set(state.out_neighbors[a1][a2]).issubset(target_set) for a2 in state.sub_assignment_p2):
+    #             stay_action_set.add(a1)
+    # if player == 1:
+    #     for a1 in state.out_neighbors:
+    #         if all(set(state.out_neighbors[a1][a2]).issubset(target_set) for a2 in state.out_neighbors[a1]):
+    #             stay_action_set.add(a1)
+
     if player == 1:
-        for a1 in state.sub_assignment_p1:
-            if all(set(state.out_neighbors[a1][a2]).issubset(target_set) for a2 in state.sub_assignment_p2):
+        for a1 in gamma_1[state]:
+            if all(set(graph.states[state].out_neighbors[a1][a2]).issubset(target_set) for a2 in gamma_2[state]):
                 stay_action_set.add(a1)
 
     # elif player == 2:
@@ -40,55 +49,82 @@ def stay(player: int, state: 'State', target_set):
     #         if flag == 0:
     #             stay_set.add(a2)
 
+    # if player == 2:
+    #     for a2 in state.sub_assignment_p2:
+    #         if all(state.out_neighbors[a1][a2].issubset(target_set) for a1 in state.sub_assignment_p1):
+    #             stay_action_set.add(a2)
+
+    # if player == 2:
+    #     for a2 in state.out_neighbors_2:
+    #         if all(state.out_neighbors[a1][a2].issubset(target_set) for a1 in state.out_neighbors_2[a2]):
+    #             stay_action_set.add(a2)
+
     if player == 2:
-        for a2 in state.sub_assignment_p2:
-            if all(state.out_neighbors[a1][a2].issubset(target_set) for a1 in state.sub_assignment_p1):
+        for a2 in gamma_2[state]:
+            if all(graph.states[state].out_neighbors[a1][a2].issubset(target_set) for a1 in gamma_1[state]):
                 stay_action_set.add(a2)
 
     return stay_action_set
 
 
-def pre(player: int, target_set, graph: Graph):
-    pre_set = set()
+def pre(player: int, target_set, graph: Graph, gamma_1: dict, gamma_2: dict):
+    # pre_set = set()
+    # if player == 1:
+    #     # TODO use s.out_neighbors[a1] instead of s.sub_assignment_p2 and use s.out_neighbors instead of s.sub_assignment_p1
+    #     return {s for state_data, s in graph.states.items() if any(
+    #         all(set(s.out_neighbors[a1][a2]).issubset(target_set) for a2 in s.out_neighbors[a1]) for a1 in
+    #         s.out_neighbors)}
+    # elif player == 2:
+    #     # return {s for state_data, s in graph.states.items() if any(
+    #     #     all(set(s.out_neighbors[a1][a2]).issubset(target_set) for a2 in s.sub_assignment_p2) for a1 in
+    #     #     s.sub_assignment_p1)}
+    #     return {s for state_data, s in graph.states.items() if any(
+    #         all(set(s.out_neighbors[a1][a2]).issubset(target_set) for a1 in s.out_neighbors_2[a2]) for a2 in
+    #         s.out_neighbors_2)}
+
     if player == 1:
         # TODO use s.out_neighbors[a1] instead of s.sub_assignment_p2 and use s.out_neighbors instead of s.sub_assignment_p1
-        return {s for state_data, s in graph.states.items() if any(
-            all(set(s.out_neighbors[a1][a2]).issubset(target_set) for a2 in s.out_neighbors[a1]) for a1 in
-            s.out_neighbors)}
+        return {state_data for state_data, s in graph.states.items() if any(
+            all(set(s.out_neighbors[a1][a2]).issubset(target_set) for a2 in gamma_2[state_data]) for a1 in
+            gamma_1[state_data])}
     elif player == 2:
-        return {s for state_data, s in graph.states.items() if any(
-            all(set(s.out_neighbors[a1][a2]).issubset(target_set) for a2 in s.sub_assignment_p2) for a1 in
-            s.sub_assignment_p1)}
+        # return {s for state_data, s in graph.states.items() if any(
+        #     all(set(s.out_neighbors[a1][a2]).issubset(target_set) for a2 in s.sub_assignment_p2) for a1 in
+        #     s.sub_assignment_p1)}
+        return {state_data for state_data, s in graph.states.items() if any(
+            all(set(s.out_neighbors[a1][a2]).issubset(target_set) for a1 in gamma_1[state_data]) for a2 in
+            gamma_2[state_data])}
 
 
-def safe(player: int, graph: Graph, target_set):
-    prev_target_set = target_set
+def safe(player: int, graph: Graph, target_set, gamma_1: dict, gamma_2: dict):
+    V_k = target_set
     while True:
-        next_target_set = prev_target_set.intersection(pre(player, prev_target_set, graph))
+        V_k_next = V_k.intersection(pre(player, V_k, graph, gamma_1, gamma_2))
 
-        if next_target_set == prev_target_set:
+        if V_k_next == V_k:
             break
-        else:
-            prev_target_set = next_target_set
+        V_k = V_k_next
 
-    return next_target_set
+    return V_k
 
 
 def almost_sure_solver(graph: Graph, final_states):
-    prev_target_set = set(graph.states.keys())
+    U_k = set(graph.states.keys())
+    gamma_1 = graph.sub_assignment_1
+    gamma_2 = graph.sub_assignment_2
 
     while True:
 
-        new_C_set = safe(2, graph, prev_target_set.difference(final_states))
-        next_target_set = safe(1, graph, prev_target_set.difference(new_C_set))
+        C_k = safe(2, graph, U_k.difference(final_states), gamma_1, gamma_2)
+        U_k_next = safe(1, graph, U_k.difference(C_k), gamma_1, gamma_2)
 
         # Updating the sub-assignments for each state
         for state_data, state in graph.states.items():
-            state.sub_assignment_p1 = stay(1, state, next_target_set)
+            if state_data not in final_states:
+                gamma_1[state_data] = stay(1, state_data, U_k_next, graph, gamma_1, gamma_2)
 
-        if next_target_set == prev_target_set:
+        if U_k_next == U_k:
             break
-        else:
-            prev_target_set = next_target_set
+        U_k = U_k_next
 
-    return next_target_set
+    return U_k, gamma_1
